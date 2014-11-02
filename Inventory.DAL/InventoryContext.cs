c7c0e830d25350258aa78da6c1776975efae5c8e
y-todorov <ytodorov@ytodorov.com>
2014-11-03 00:17:16 +0200

    CreateDatabaseIfNotExists

--- Inventory.DAL/InventoryContext.cs
+++ Inventory.DAL/InventoryContext.cs
@@ -21,8 +21,14 @@ namespace Inventory.DAL
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<InventoryContext>());
+            /*
+            There are four different database Initialization strategies:
 
+CreateDatabaseIfNotExists: This is default initializer. As name suggests, it will create the database if none exists as per the configuration. However, if you change the model class and then run the application with this initializer, then it will throw an exception.
+DropCreateDatabaseIfModelChanges: This initializer drops an existing database and creates a new database, if your model classes (entity classes) have been changed. So you don’t have to worry about maintaining your database schema, when your model classes change.
+DropCreateDatabaseAlways: As the name suggests, this initializer drops an existing database every time you run the application, irrespective of whether your model classes have changed or not. This will be useful, when you want fresh database, every time you run the application, while you are developing the application.
+Custom DB Initializer: You can also create your own custom initializer, if any of the above don't satisfy your requirements or you want to do some other process that initializes the database using above initializer.
+            */
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); 
             modelBuilder.Conventions.Add<DateTime2Convention>();
 
