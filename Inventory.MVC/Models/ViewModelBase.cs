7008cda48c6edbc2a74cc182e68b6ad5f243ad90
y-todorov <ytodorov@ytodorov.com>
2014-11-02 12:28:55 +0200

    custom editor for created on adn modified on

--- Inventory.MVC/Models/ViewModelBase.cs
+++ Inventory.MVC/Models/ViewModelBase.cs
@@ -11,35 +11,35 @@ namespace Inventory.MVC.Models
 {
     public class ViewModelBase
     {
-        //[ReadOnly(true)]
-        [HiddenInput(DisplayValue = false)] // временно
+        [ReadOnly(true)]
+        [HiddenInput(DisplayValue = false)]
         [Display(ResourceType = typeof(ViewModelResources),
        Name = "ViewModel_Id",
        Order = -1000)]
         public long Id { get; set; }
 
-        //[ReadOnly(true)]
-        //[HiddenInput(DisplayValue = false)] // временно
+        [UIHint("DateCreatedOnModifiedOn")]
+        [ReadOnly(true)]
+        [HiddenInput(DisplayValue = false)]
         [DataType(DataType.DateTime)]
         [Display(ResourceType = typeof(ViewModelResources),
             Name = "ViewModelBase_CreatedOn_Name")]
         public DateTime? CreatedOn { get; set; }
 
-        //[ReadOnly(true)]
-        [HiddenInput(DisplayValue = false)] // временно
+        [HiddenInput(DisplayValue = false)]
         [Display(ResourceType = typeof(ViewModelResources),
     Name = "ViewModelBase_CreatedBy_Name")]
         public string CreatedBy { get; set; }
 
-        //[ReadOnly(true)]
-        //[HiddenInput(DisplayValue = false)] // временно
+        [UIHint("DateCreatedOnModifiedOn")]
+        [ReadOnly(true)]
+        [HiddenInput(DisplayValue = false)]
         [DataType(DataType.DateTime)]
         [Display(ResourceType = typeof(ViewModelResources),
         Name = "ViewModelBase_ModifiedOn_Name")]
         public DateTime? ModifiedOn { get; set; }
 
-        //[ReadOnly(true)]
-        [HiddenInput(DisplayValue = false)] // временно
+        [HiddenInput(DisplayValue = false)]
         [Display(ResourceType = typeof(ViewModelResources),
     Name = "ViewModelBase_ModifiedBy_Name")]
         public string ModifiedBy { get; set; }
