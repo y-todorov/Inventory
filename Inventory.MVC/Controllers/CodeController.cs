0c4258f0a3ddd35aaf50d8c219098cde7ddd3cda
y-todorov <ytodorov@ytodorov.com>
2014-11-04 00:37:03 +0200

    CodeController and images in Content

---
+++ Inventory.MVC/Controllers/CodeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory.MVC.Controllers
+{
+    public class CodeController : Controller
+    {
+        public ActionResult BarCode()
+        {
+            return View();
+        }
+
+        public ActionResult QrCode()
+        {
+            return View();
+        }
+
+    }
+}
